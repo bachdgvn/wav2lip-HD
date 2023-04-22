dd7a364c2707363d3a5dc552f304b0903d902fe6
anothermartz <89461919+anothermartz@users.noreply.github.com>
2023-04-22 13:55:53 +0100

    Made loading bar better

--- GFPGAN-master/inference_gfpgan.py
+++ GFPGAN-master/inference_gfpgan.py
@@ -114,13 +114,11 @@ def main():
         bg_upsampler=bg_upsampler)
 
     # ------------------------ restore ------------------------
-    for img_path in tqdm(img_list):
+    for img_path in tqdm(img_list, desc='Processing'):
         # read image
         img_name = os.path.basename(img_path)
-        print(f'Processing {img_name} ...')
         basename, ext = os.path.splitext(img_name)
         input_img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-
         # restore faces and background if necessary
         cropped_faces, restored_faces, restored_img = restorer.enhance(
             input_img, has_aligned=args.aligned, only_center_face=args.only_center_face, paste_back=True)
